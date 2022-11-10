ca6f4049eb4be083aa994285a89481330e8877f9
Poong-hyerim <poonghrdd@gmail.com>
2022-11-10 22:10:20 +0900

    feat:카카오맵 api 초기 setup

--- app/src/main/java/com/samdoreee/fieldgeolog/MainActivity.kt
+++ app/src/main/java/com/samdoreee/fieldgeolog/MainActivity.kt
@@ -1,37 +1,37 @@
 package com.samdoreee.fieldgeolog
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.samdoreee.fieldgeolog.ui.theme.FieldGeoLogTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FieldGeoLogTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.main)
     }
+
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    println("Hello $name!")
 }
 
 @Preview(showBackground = true)
