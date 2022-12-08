fe415f182be5d12258065a6ccef0f495b5a8c12a
Poong-hyerim <poonghrdd@gmail.com>
2022-12-09 08:37:20 +0900

    feat: post/memolist 입력 연결

--- app/src/main/java/com/samdoreee/fieldgeolog/DetailActivity.kt
+++ app/src/main/java/com/samdoreee/fieldgeolog/DetailActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.samdoreee.fieldgeolog.databinding.ActivityDetailBinding
+import com.samdoreee.fieldgeolog.databinding.ActivityWriteBinding
+import com.samdoreee.fieldgeolog.network.Memo
+import com.samdoreee.fieldgeolog.network.Spot
+
+lateinit var binding: ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
 
         val btn_back_to_list = findViewById<Button>(R.id.btn_back_to_list)
         btn_back_to_list.setOnClickListener {
