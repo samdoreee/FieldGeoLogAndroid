c5b732d991db61d00b788113b88ab74e8dd23a74
Jeonghyeon Kim <49496217+JeonghyeonK@users.noreply.github.com>
2023-09-30 15:20:25 +0900

    feature: 로그인 기능 병합 (#23)
    * 로그인 기능 병합
    
    * feature: 로그인 기능 병합

--- app/src/main/java/com/samdoreee/fieldgeolog/MainActivity.kt
+++ app/src/main/java/com/samdoreee/fieldgeolog/MainActivity.kt
@@ -3,11 +3,13 @@ package com.samdoreee.fieldgeolog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
+import com.kakao.sdk.user.UserApiClient
 import com.samdoreee.fieldgeolog.R
 import com.samdoreee.fieldgeolog.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapView
@@ -36,6 +38,20 @@ class MainActivity : AppCompatActivity() {
         binding.mypagebtn.setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
+        }
+
+        // 로그에 로그인 정보 데이터 출력
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e(Constants.TAG, "사용자 정보 요청 실패 $error")
+            } else if (user != null) {
+                Log.d(Constants.TAG, "사용자 정보 요청 성공 : $user")
+
+
+            // 아래 코드처럼 데이터 활용하면 됨
+            // binding.txtNickName.text = user.kakaoAccount?.profile?.nickname
+
+            }
         }
 
 
